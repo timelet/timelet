4841a0daac60746684ece468f9b89a15f4f966bb
Robin <1105080+openscript@users.noreply.github.com>
2021-01-20 09:20:07 +0000

    Clean up scaffold

--- src/views/App.test.tsx
+++ src/views/App.test.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-import { expect } from 'chai';
 import App from './App';
 
 describe('<App>', () => {
-  it('renders learn react link', () => {
+  it('renders the App', () => {
     const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(document.body.contains(linkElement));
+    // const linkElement = getByText(/learn react/i);
+    // expect(document.body.contains(linkElement));
   });
 });
---
+++ src/views/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface AppProps {}
+
+export default function App({}: AppProps) {
+  return (
+    <div className="App">
+      Timelet
+    </div>
+  );
+}
