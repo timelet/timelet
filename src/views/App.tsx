f8d6eb3787ad4822b878f8b445419ec298af8268
Robin <1105080+openscript@users.noreply.github.com>
2021-01-20 10:40:17 +0000

    Add continuous integration and deployment

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -3,9 +3,5 @@ import React from 'react';
 interface AppProps {}
 
 export default function App({}: AppProps) {
-  return (
-    <div className="App">
-      Timelet
-    </div>
-  );
+  return <div className="App">Timelet</div>;
 }
