bf96d7837b2bf775396b556f341d5260f0b567a2
Robin <1105080+openscript@users.noreply.github.com>
2021-01-23 18:57:42 +0000

    Cache google fonts by serviceworker

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,28 @@
 module.exports = {
   "globDirectory": "build/",
   "globPatterns": [
-    "**/*.{js,json,css,ico,html,txt}"
+    "**/*.{js,json,css,png,html,txt}"
   ],
-  "swDest": "build/sw.js"
+  "swDest": "build/sw.js",
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'google-fonts-stylesheets'
+      }
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts-webfonts',
+        expiration: {
+          // cache fonts for 7 days
+          maxAgeSeconds: 60 * 60 * 24 * 7,
+          maxEntries: 30
+        }
+      }
+    }
+  ]
 };
