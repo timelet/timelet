ec85b9ffe97620edfd2c19f8309a5f4085dbaa34
Robin <1105080+openscript@users.noreply.github.com>
2021-01-23 20:06:51 +0000

    Configure Material UI

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,8 +1,16 @@
+import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+import styled from '@emotion/styled';
 import { DatabaseProvider } from '../contexts/DatabaseContext';
 import type { TimeletDatabase } from '../database';
 import { initializeDatabase } from '../database';
 import Entries from './Entries';
+
+const Title = styled(Typography)`
+  margin-left: 1rem;
+  flex-grow: 1;
+`;
 
 export default function App() {
   const [database, setDatabase] = useState<TimeletDatabase>();
@@ -18,6 +26,14 @@ export default function App() {
 
   return (
     <DatabaseProvider database={database}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Title variant="h6">Timelet</Title>
+        </Toolbar>
+      </AppBar>
       <Entries />
     </DatabaseProvider>
   );
