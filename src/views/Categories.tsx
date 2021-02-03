fe11a91a3b74580ceb9d1f74125436785e4e298c
Robin <1105080+openscript@users.noreply.github.com>
2021-02-03 13:01:06 +0000

    Add content elements

--- src/views/Categories.tsx
+++ src/views/Categories.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import ContentContainer from '../layout/default/ContentContainer';
+import ContentElement from '../layout/default/ContentElement';
+import ContentTitle from '../layout/default/ContentTitle';
 
 export default function Categories() {
-  return <div>Categories</div>;
+  return (
+    <ContentContainer>
+      <ContentTitle>
+        <FormattedMessage id="title.categories" defaultMessage="Categories" />
+      </ContentTitle>
+      <ContentElement>Categories</ContentElement>
+    </ContentContainer>
+  );
 }
