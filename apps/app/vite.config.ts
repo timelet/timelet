ed19213b4783eccd91aa149d8bdccfce9739190a
Robin Bühler <git+r@obin.ch>
2023-03-21 15:19:01 +0000

    chore: configure pwa

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA as pwa } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    pwa({
+      manifest: {
+        name: "Timelet",
+        short_name: "Timelet",
+        description: "Distributed collaborative offline-first time tracking.",
+        theme_color: "#e6e6e6",
+        background_color: "#e6e6e6",
+        display: "standalone",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
