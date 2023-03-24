4b3cbb43d842b6e7830054fef0460753d9544d9d
Robin Bühler <git+r@obin.ch>
2023-03-24 16:07:10 +0000

    chore: setup localization

--- apps/app/src/layouts/default/DefaultFooter.tsx
+++ apps/app/src/layouts/default/DefaultFooter.tsx
@@ -1,6 +1,11 @@
 import { Footer } from "@timelet/ui";
+import { FormattedMessage } from "react-intl";
 import { CONFIGURATION } from "../../configuration";
 
 export function DefaultFooter() {
-  return <Footer height={48}>{CONFIGURATION.VERSION}</Footer>;
+  return (
+    <Footer height={48}>
+      <FormattedMessage id="layout.footer.version" values={{ version: CONFIGURATION.VERSION }} />
+    </Footer>
+  );
 }
--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,13 +1,19 @@
 import { TimeletUIProvider } from "@timelet/ui";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { IntlProvider } from "react-intl";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
+import enUSMessages from "../../../assets/localization/en-US.json";
+
+const messages = enUSMessages;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <TimeletUIProvider withGlobalStyles withNormalizeCSS>
-      <RouterProvider router={router} />
-    </TimeletUIProvider>
+    <IntlProvider messages={messages} locale="en-US" defaultLocale="en-US">
+      <TimeletUIProvider withGlobalStyles withNormalizeCSS>
+        <RouterProvider router={router} />
+      </TimeletUIProvider>
+    </IntlProvider>
   </React.StrictMode>
 );
