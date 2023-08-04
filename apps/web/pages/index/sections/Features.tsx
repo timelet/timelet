cdd88005dcbfc2a3ab3d689c44ab6202fb264c90
Robin Bühler <git+r@obin.ch>
2023-08-04 07:22:03 +0000

    chore: add some styles to the features section

--- apps/web/pages/index/sections/Features.tsx
+++ apps/web/pages/index/sections/Features.tsx
@@ -1,4 +1,18 @@
+import { Card, Group, Spoiler, Text } from "@timelet/ui";
 import { FeaturesType } from "../../../data/features";
+import { Theme, css } from "@emotion/react";
+
+const featuresStyles = (theme: Theme) => css`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+
+  @media (max-width: ${theme.breakpoints.xl}) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (max-width: ${theme.breakpoints.sm}) {
+    grid-template-columns: 1fr;
+  }
+`;
 
 type FeaturesProps = {
   features: FeaturesType["features"];
@@ -6,14 +20,18 @@ type FeaturesProps = {
 
 export function Features({ features }: FeaturesProps) {
   return (
-    <section>
+    <section css={featuresStyles}>
       {features.map((f) => (
-        <article key={f.id}>
-          <header>
+        <Card component="article" key={f.id}>
+          <Card.Section>
             <h3>{f.title}</h3>
-            <p>{f.description}</p>
-          </header>
-        </article>
+          </Card.Section>
+          <Group>
+            <Spoiler maxHeight={72} showLabel="More" hideLabel="Less">
+              <Text>{f.description}</Text>
+            </Spoiler>
+          </Group>
+        </Card>
       ))}
     </section>
   );
