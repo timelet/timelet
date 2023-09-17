cacd7bbf612699f9bedc52ab75c43779f2ddbe5f
Robin <git+r@obin.ch>
2023-09-17 01:24:26 +0000

    fix: app linking from website

--- apps/web/renderer/configuration.ts
+++ apps/web/renderer/configuration.ts
@@ -3,7 +3,7 @@ import packageJson from "../package.json";
 export const CONFIGURATION = {
   VERSION: packageJson.version,
   PATHS: {
-    APP: import.meta.env.DEV ? "http://localhost:3001" : "./app",
+    APP: import.meta.env.DEV ? "http://localhost:3001" : "/app",
     CODE: "https://github.com/timelet/timelet",
     DOCS: "/docs/",
   },
