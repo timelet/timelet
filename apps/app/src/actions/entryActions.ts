535d1377675c2a6ed443b400d9dda7925dc86a38
Robin Bühler <git+r@obin.ch>
2024-01-05 10:43:41 +0000

    chore: remove action

--- apps/app/src/actions/entryActions.ts
+++
@@ -1,8 +0,0 @@
-import { useDatabase } from "../persistence/DatabaseProvider";
-import { EntryDocument } from "../persistence/collections/entryCollection";
-
-export function createEntryAction(entry: EntryDocument) {
-  return useDatabase()?.collections.entries.insert(entry);
-}
-
-export function indexEntriesAction() {}
