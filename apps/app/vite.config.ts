18ca93db7a86e01d66671c38d73c4dc63e1ef5d7
Robin Bühler <git+r@obin.ch>
2024-01-06 17:21:59 +0000

    chore: remove emotion dependencies

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3001,
   },
   plugins: [
-    react({ jsxImportSource: "@emotion/react" }),
+    react(),
     pwa({
       manifest: {
         name: "Timelet",
