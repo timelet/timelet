d9249337e6a612c3d2d8c3b5dd178d6f0e0a5bcb
Robin Bühler <git+r@obin.ch>
2024-03-28 15:39:32 +0000

    chore: enhance error pages

--- apps/web/pages/_error/+Page.tsx
+++ apps/web/pages/_error/+Page.tsx
@@ -1,17 +1,17 @@
 export function Page({ is404 }: { is404: boolean }) {
   if (is404) {
     return (
-      <>
+      <section>
         <h1>404 Page Not Found</h1>
         <p>This page could not be found.</p>
-      </>
+      </section>
     );
   } else {
     return (
-      <>
+      <section>
         <h1>500 Internal Server Error</h1>
         <p>Something went wrong.</p>
-      </>
+      </section>
     );
   }
 }
