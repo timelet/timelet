d5b042c7092626e71c0744c3bf23350060882122
Robin Bühler <git+r@obin.ch>
2024-04-14 09:35:16 +0000

    fix: docs before prerender start

--- apps/web/pages/index/docs/+onBeforePrerenderStart.ts
+++ apps/web/pages/index/docs/+onBeforePrerenderStart.ts
@@ -1,6 +1,6 @@
 import { glob } from "glob";
 
 export async function onBeforePrerenderStart() {
-  const paths = await glob("../../../../../assets/content/en-US/**/*.mdx");
-  return paths.map((p) => p.replace("../../../../../assets/content/en-US", "").replace(".mdx", "").replace("index", ""));
+  const paths = await glob("../../assets/content/en-US/**/*.mdx");
+  return paths.map((p) => p.replace("../../assets/content/en-US", "").replace(".mdx", "").replace("index", ""));
 }
