e146a4546a64f7df84b98cb21da917d7b3b83920
Robin Bühler <git+r@obin.ch>
2024-04-21 18:54:16 +0000

    chore: parse locale from url

--- apps/web/renderer/utils/locale.test.ts
+++ apps/web/renderer/utils/locale.test.ts
@@ -1,4 +1,4 @@
-import { findClosestLocale, parseFilenameSuffix } from "./locale";
+import { findClosestLocale, parseLocaleFromFilenameSuffix, parseLocaleFromURL } from "./locale";
 import { describe, it, expect } from "vitest";
 
 describe("findClosestLocale", () => {
@@ -15,27 +15,54 @@ describe("findClosestLocale", () => {
 
 describe("parseFilenameSuffix", () => {
   it("should estimate the language in a filename", () => {
-    const { estimatedLocale } = parseFilenameSuffix("index.de.md", "en");
+    const { estimatedLocale } = parseLocaleFromFilenameSuffix("index.de.md", "en");
     expect(estimatedLocale).toBe("de");
   });
 
   it("should estimate the exact locale in a filename", () => {
-    const { estimatedLocale } = parseFilenameSuffix("index.de-CH.md", "en");
+    const { estimatedLocale } = parseLocaleFromFilenameSuffix("index.de-CH.md", "en");
     expect(estimatedLocale).toBe("de-CH");
   });
 
   it("should return the default locale if no locale was found in filename", () => {
-    const { estimatedLocale } = parseFilenameSuffix("index.md", "en");
+    const { estimatedLocale } = parseLocaleFromFilenameSuffix("index.md", "en");
     expect(estimatedLocale).toBe("en");
   });
 
   it("should return the filename without extensions, even when there is no locale", () => {
-    const { filename } = parseFilenameSuffix("index.md", "en");
+    const { filename } = parseLocaleFromFilenameSuffix("index.md", "en");
     expect(filename).toBe("index");
   });
 
   it("should return the filename without extensions", () => {
-    const { filename } = parseFilenameSuffix("index.en-US.md", "en");
+    const { filename } = parseLocaleFromFilenameSuffix("index.en-US.md", "en");
     expect(filename).toBe("index");
+  });
+});
+
+describe("parseLocaleFromURL", () => {
+  it("should parse the locale from a URL", () => {
+    const locale = parseLocaleFromURL("/en-US/docs/getting-started", "en");
+    expect(locale).toBe("en-US");
+  });
+
+  it("should parse the locale from a URL with trailing slash", () => {
+    const locale = parseLocaleFromURL("/en-US/", "en");
+    expect(locale).toBe("en-US");
+  });
+
+  it("should parse the locale from a URL without trailing slashes", () => {
+    const locale = parseLocaleFromURL("/en-US", "en");
+    expect(locale).toBe("en-US");
+  });
+
+  it("should parse the short form locale from a URL", () => {
+    const locale = parseLocaleFromURL("/en", "de");
+    expect(locale).toBe("en");
+  });
+
+  it("should return the default locale if no locale was found in URL", () => {
+    const locale = parseLocaleFromURL("/", "en");
+    expect(locale).toBe("en");
   });
 });
--- apps/web/renderer/utils/locale.ts
+++ apps/web/renderer/utils/locale.ts
@@ -1,4 +1,5 @@
 const FILENAME_SUFFIX_PATTERN = /^([^.]+)\.?(.*)?(?=\.\w+)/;
+const IETF_BCP_47_LOCALE_PATTERN = /^\/?(\w{2}(-\w{2,})*)\/?/;
 
 export function findClosestLocale(locale: string, locales: string[]) {
   if (locale.length === 5) {
@@ -7,10 +8,15 @@ export function findClosestLocale(locale: string, locales: string[]) {
   return locales.find((l) => l.indexOf(locale) !== -1);
 }
 
-export const parseFilenameSuffix = (name: string, defaultLocale: string) => {
+export const parseLocaleFromFilenameSuffix = (name: string, defaultLocale: string) => {
   const nameMatch = name.match(FILENAME_SUFFIX_PATTERN);
   const filename = nameMatch && nameMatch[1] ? nameMatch[1] : name;
   const estimatedLocale = nameMatch && nameMatch[2] ? nameMatch[2] : defaultLocale;
 
   return { filename, estimatedLocale };
+};
+
+export const parseLocaleFromURL = (url: string, defaultLocale: string) => {
+  const localeMatch = url.match(IETF_BCP_47_LOCALE_PATTERN);
+  return localeMatch ? localeMatch[1] : defaultLocale;
 };
