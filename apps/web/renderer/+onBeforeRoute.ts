02e9cb08ca00dad68e5cf2b030998712c9096f13
Robin Bühler <git+r@obin.ch>
2024-05-06 15:47:46 +0000

    feat: i18n

--- apps/web/renderer/+onBeforeRoute.ts
+++ apps/web/renderer/+onBeforeRoute.ts
@@ -1,15 +1,17 @@
 import { OnBeforeRouteSync } from "vike/types";
 import { findClosestLocale, splitLocaleFromURL } from "./utils/locale";
 import { CONFIGURATION } from "./configuration";
+import { urlToString } from "./utils/path";
 
 export const onBeforeRoute: OnBeforeRouteSync = (pageContext) => {
   const { locale, path } = splitLocaleFromURL(pageContext.urlParsed.pathname, CONFIGURATION.DEFAULT_LOCALE);
   const closestLocale = findClosestLocale(locale, Object.keys(CONFIGURATION.LOCALES)) || CONFIGURATION.DEFAULT_LOCALE;
+  const urlOriginal = urlToString(pageContext.urlParsed);
   const internalLocale = CONFIGURATION.LOCALES[closestLocale as keyof typeof CONFIGURATION.LOCALES];
-  const urlLogical = pageContext.urlOriginal.replace(pageContext.urlParsed.pathname, path);
+  const urlLogical = urlOriginal.replace(pageContext.urlParsed.pathname, path);
   const availableLocales = Object.entries(CONFIGURATION.LOCALES).map(([_key, value]) => ({
     locale: value,
-    path: pageContext.urlOriginal.replace(pageContext.urlParsed.pathname, `/${value.slug}${path}`),
+    path: urlOriginal.replace(pageContext.urlParsed.pathname, `/${value.slug}${path}`),
   }));
 
   return { pageContext: { locale: internalLocale, availableLocales, urlLogical } };
--- apps/web/renderer/utils/path.ts
+++ apps/web/renderer/utils/path.ts
@@ -1,6 +1,11 @@
+import { Url } from "vike/types";
 import { CONFIGURATION } from "../configuration";
 
 export function stripContentPath(path: string, locale?: keyof typeof CONFIGURATION.LOCALES) {
   const contentPath = locale ? `${CONFIGURATION.PATHS.CONTENT}/${locale}/` : `${CONFIGURATION.PATHS.CONTENT}/`;
   return path.replace(contentPath, "").replace(".mdx", "").replace("index", "");
+}
+
+export function urlToString({ origin, pathname, searchOriginal }: Url) {
+  return `${origin || ""}${pathname}${searchOriginal || ""}`;
 }
