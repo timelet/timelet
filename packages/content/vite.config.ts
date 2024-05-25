7fdcde2053a0c060f7f745dad23b13d341d8bc08
Robin Bühler <git+r@obin.ch>
2024-05-25 12:11:32 +0000

    chore: prepare @timelet/content for publication

--- packages/content/vite.config.ts
+++ packages/content/vite.config.ts
@@ -10,6 +10,19 @@ export default defineConfig({
       entry: "src/index.ts",
       name: packageJson.name,
     },
+    rollupOptions: {
+      external: ["fast-glob", "node:path", "node:crypto", "to-vfile", "vfile-matter", "limax"],
+      output: {
+        globals: {
+          "fast-glob": "glob",
+          "node:path": "nodePath",
+          "node:crypto": "nodeCrypto",
+          "to-vfile": "toVfile",
+          "vfile-matter": "vfileMatter",
+          limax: "slugify",
+        },
+      },
+    },
   },
   plugins: [
     dts({
